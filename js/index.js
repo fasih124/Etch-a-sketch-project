ff2fa4be10f0447ad22352786d6341016302d2ff
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-28 17:44:52 +0500

    Complete all requirment & website

--- js/index.js
+++ js/index.js
@@ -1,4 +1,37 @@
-let num = 16;
-for (let i = 0; i < num; i++) {
-  for (let j = 0; j < num; j++) {}
+let size = 16;
+let color = "black";
+const container = document.querySelector(".container");
+
+function createGrid(num) {
+  for (let i = 0; i < num; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < num; j++) {
+      const divs = document.createElement("div");
+      row.appendChild(divs);
+      divs.addEventListener("mouseover", () => {
+        divs.style.backgroundColor = color;
+      });
+    }
+    container.appendChild(row);
+  }
 }
+
+createGrid(size);
+
+const sizeBtn = document.querySelector("#size");
+sizeBtn.addEventListener("click", () => {
+  let userSize = prompt("Enter the Size of Grid(1-100): ");
+  if (userSize <= 0 || userSize > 100) {
+    alert("invalid Size(setting Size 16)");
+    userSize = 16;
+  }
+  size = Number(userSize);
+  const removedivs = document.querySelectorAll(".row");
+  removedivs.forEach((div) => {
+    container.removeChild(div);
+  });
+  createGrid(size);
+});
+
+// buttons
